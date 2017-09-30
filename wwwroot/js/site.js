2e322ec435dc96c80ef839ef71bfd81bdbde9828
Slavisa Karalic <slavisa.karalic@gmail.com>
2017-09-30 16:56:19 +0200

    Day Night Modus, persistant

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,10 +1,24 @@
 ﻿// Write your JavaScript code.
+if (localStorage.getItem("modus")=== "night"){
+    $(".main").addClass("night");
+    $(".change-style").text("Day Modus");    
+} else {
+    $(".main").removeClass("night");
+    $(".change-style").text("Night Modus");    
+}
+
 $(".change-style").click(()=>{
-    var night = 'night';
+    var night = "night";
     var main = $(".main");
-    if(main.hasClass(night)) {
-        main.removeClass("night");
+    
+   if(main.hasClass(night)) {  
+     main.removeClass("night");
+     $(".change-style").text("Night Modus");
+     localStorage.setItem("modus", "day");
     } else {
-        main.addClass("night");
+     main.addClass("night");
+     $(".change-style").text("Day Modus");
+     localStorage.setItem("modus", "night");
     }
 });
+
