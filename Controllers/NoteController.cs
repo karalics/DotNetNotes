b558021a06c636e2a6aff54f97d9aa4eeefd72b7
Robin Ratcliff <robin.ratcliff@hsr.ch>
2017-10-08 20:48:50 +0200

    DueDate Required and some cleanup with imports

--- Controllers/NoteController.cs
+++ Controllers/NoteController.cs
@@ -1,6 +1,4 @@
 using System.Linq;
-using System.Threading;
-using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using DotNetNotes.Models;
--- Models/Note.cs
+++ Models/Note.cs
@@ -1,19 +1,15 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.AspNetCore.Mvc.DataAnnotations;
 namespace DotNetNotes.Models
 {
-     public class Note
+    public class Note
     {
         public int Id { get; set; }
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 3)]
         [Display(Name = "Title")]
         public string Title { get; set; }
+        [Required]
         public DateTime DueDate { get; set; }
         [Required]
         [Range(1,5, ErrorMessage="The {0} must be between between 1 and 5.")]
